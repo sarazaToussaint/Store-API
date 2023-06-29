ab644f1336d271eb6bf8d3afd4af56a30c4563b5
sarazaToussaint <tmsaraza@gmail.com>
2023-06-29 17:47:27 +0200

    Create a model for the API

---
+++ starter/controllers/products.js
@@ -0,0 +1,15 @@
+
+
+const getAllProductsStatic = async (req, res) => {
+//   throw new Error('testing async error');
+  res.status(200).json({ msg: 'Products setting route' });
+}
+
+const getAllProducts = async (req, res) => {
+    res.status(200).json({ msg: 'Products route' });
+}
+
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic
+};
