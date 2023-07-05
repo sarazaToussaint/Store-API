478d204debb8425947757f65087db6f4a83d5803
sarazaToussaint <tmsaraza@gmail.com>
2023-07-05 15:57:30 +0200

    Populate the database with products

--- starter/populate.js
+++ starter/populate.js
@@ -9,6 +9,8 @@ const start = async () => {
   
   try {
     await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    await Product.create(jsonProduct);
     console.log('Success!!!');
   } catch (error) {
     console.log(error);
